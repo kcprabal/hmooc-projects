7dbfc81bc86eb4298185a5db5889c437dd773ae4
vagrant <vagrant@precise64.(none)>
2014-10-08 14:28:24 +0800

    first commit

---
+++ cs50/credit.c
@@ -0,0 +1,8 @@
+#include <stido.h>
+long long int GetLongLong();
+int main (int argc, char * [] argv){
+    long long int credit_card = GetLongLong 
+}
+long long int GetLongLong(){
+      
+}
---
+++ readme.md
@@ -0,0 +1,5 @@
+# The repository for harvard open course
+1. [cs50](cs50.net)
+2. [cs75](cs75.net)
+
+## Hope to insist and have fun on these projects.
