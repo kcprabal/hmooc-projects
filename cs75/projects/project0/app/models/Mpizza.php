c34e0ef1e2690bb15a18a04a58aa6df92e713fef
hans <lizihan0328@gmail.com>
2014-10-13 18:11:55 +0800

    small changes in model

--- cs75/projects/project0/app/models/Mpizza.php
+++ cs75/projects/project0/app/models/Mpizza.php
@@ -7,11 +7,20 @@ class Mpizza{
     public $xml;
     
     public function __construct(){
+        $this -> xml = simplexml_load_file(MODELS_PATH."data.xml");
+    }
+    public function getDat(){
+        return $this-> xml;
+    }
+    public function getPizzaName(){
+        $data = $this -> 
+        return $data;
+    }
+    public function getPizzaDescription(){
 
     }
-    public function loadData(){
-        $this -> xml = simplexml_load_file(MODELS_PATH."data.xml");
-        return $this-> xml;
+    public function getPizzaSize($size){
+
     }
 }
 
